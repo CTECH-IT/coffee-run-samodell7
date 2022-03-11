31ae9d501cf070ed43342745530e0e5688eed235
Sam Odell <7421902@isd535.org>
2022-03-11 11:33:02 -0600

    started working. fix myTruck

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,4 +1,4 @@
-(function (window){
+(function (window) {
 'use strict';
 
 let App = window.App || {};
@@ -9,26 +9,27 @@ function FormHandler(selector) {
     if (!selector) {
         throw new Error('No selector provided!');
     }
-}
+
 
 //find the "selector" and assign to this.formElement
 this.$formElement = $(selector);
 if (this.$formElement.length == 0) {
     throw new Error('Could not find element with selector: ' + selector);
 }
+}
 
-FormHandler.prototype.addSubmitHandler = function () {
+FormHandler.prototype.addSubmitHandler = function (func) {
     console.log('Setting the submit handler for the form...');
     this.$formElement.on('submit', function(event) {
         event.preventDefault();
 
-        
-        let data= {};
-        $(this).serializArray().forEach(function (item) {
+        let data = {};
+        $(this).serializeArray().forEach(function (item) {
             data[item.name] = item.value;
             console.log(item.name + ' is ' + item.value);
         });
         console.log(data);
+        func(data); //call function passed down in data from form
     });
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 (function (window){
     'use strict';
 
+    const FORM_SELECTOR = '[data-coffee-order="form"]';
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let FormHandler = App.FormHandler;
 
     let myTruck = new Truck('12345', new DataStore());
 
     window.myTruck = myTruck;
+
+    let formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler.addSubmitHandler(myTurck.createOrder.bind(myTruck));
+    console.log(formHandler);
 
 })(window);
