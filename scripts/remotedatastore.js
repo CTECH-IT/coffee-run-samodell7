7eba5ad4373bde29bf42f6ff6451439ed0e41ab0
Sam Odell <7421902@isd535.org>
2022-03-18 11:27:03 -0500

    working on broken unit

---
+++ scripts/remotedatastore.js
@@ -0,0 +1,39 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+    function RemoteDataStore(url) {
+        if (!url) {
+            throw new Error('No remote URL supplied.');
+        }
+        this.serverUrl = url;
+    }
+    RemoteDataStore.prototype.add = function (key,val) {
+        //code
+        $.post(this.serverUrl, val, function (serverResponse) {
+            console.log(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.getAll = function (cb) {
+        //code
+        //sum stuff
+        $.get(this.serverUrl, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.get = function (key, cb) {
+        // make a get call pass as email
+        //return as one order
+        // call the function cb
+        $.get(this.serverUrl + '?emailAddress=' + key, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
+    App.RemoteDataStore = RemoteDataStore;
+    window.App = App;
+})(window);
